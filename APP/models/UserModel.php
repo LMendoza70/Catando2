25caa21a7f97c2db19e262727e3df0e70f720cb8
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-07-10 13:15:08 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   APP/models/UserModel.php
    
     Changes not staged for commit:
    	modified:   APP/config/BDConection.php
    	modified:   APP/controller/PresentsController.php
    	modified:   APP/controller/ProductsController.php
    	modified:   APP/controller/UserController.php
    	modified:   APP/controller/VarietalsController.php
    	modified:   APP/controller/index.php
    	deleted:    APP/src/img/avatars/Avatar_64a71093322d7.jpeg
    	deleted:    APP/src/img/avatars/Avatar_64a710ac752cc.jpeg
    	modified:   APP/views/Users/userEditView.php
    	modified:   APP/views/Users/userIndexView.php
    	modified:   APP/views/layoutLog.php
    
     Untracked files:
    	APP/src/img/avatars/Avatar_64ac4fea0d45f.png
    	APP/src/img/avatars/Avatar_64ac50f161973.png

--- APP/models/UserModel.php
+++ APP/models/UserModel.php
@@ -1,162 +1,161 @@
 <?php
-    class UserModel{
+class UserModel
+{
+    //creamos la instancia de la coneccion
+    private $bdConection;
+
+    //creamos el constructor para conectar a la base de datos
+    public function __construct()
+    {
+        //llamamos a la clase de la coneccion
+        require_once("APP/config/bdConection.php");
         //creamos la instancia de la coneccion
-        private $bdConection;
+        $this->bdConection = new BDConection();
+    }
 
-        //creamos el constructor para conectar a la base de datos
-        public function __construct(){
-            //llamamos a la clase de la coneccion
-            require_once("APP/config/bdConection.php");
-            //creamos la instancia de la coneccion
-            $this->bdConection = new BDConection();
+    //creamos la funcion para obtener todos los usuarios
+    public function getAll()
+    {
+        //creamos la consulta
+        $sql = "SELECT * FROM user";
+        //obtenemos la coneccion
+        $connection = $this->bdConection->getConnection();
+        //ejecutamos la consulta
+        $result = $connection->query($sql);
+        //creamos el arreglo para guardar los usuarios
+        $users = array();
+        //recorremos el resultado de la consulta con la funcion fetch_assoc()
+        while ($user = $result->fetch_assoc()) {
+            //agregamos cada usuario al arreglo
+            $users[] = $user;
         }
+        //cerramos la coneccion
+        $this->bdConection->closeConnection();
+        //retornamos el arreglo con todos los usuarios
+        return $users;
+    }
 
-        //creamos la funcion para obtener todos los usuarios
-        public function getAll(){
-            //creamos la consulta
-            $sql = "SELECT * FROM user";
-            //obtenemos la coneccion
-            $connection = $this->bdConection->getConnection();
-            //ejecutamos la consulta
-            $result = $connection->query($sql);
-            //creamos el arreglo para guardar los usuarios
-            $users = array();
-            //recorremos el resultado de la consulta con la funcion fetch_assoc()
-            while($user = $result->fetch_assoc()){
-                //agregamos cada usuario al arreglo
-                $users[] = $user;
-            }
-            //cerramos la coneccion
-            $this->bdConection->closeConnection();            
-            //retornamos el arreglo con todos los usuarios
-            return $users;
+    //creamos la funcion para obtener un usuario por su id
+    public function getById($id)
+    {
+        //creamos la consulta
+        $sql = "SELECT * FROM user WHERE IdUser = " . $id;
+        //obtenemos la coneccion
+        $connection = $this->bdConection->getConnection();
+        //ejecutamos la consulta
+        $result = $connection->query($sql);
+        //verificamos que la consulta haya traido un resultado
+        if ($result && $result->num_rows > 0) {
+            //obtenemos el usuario
+            $user = $result->fetch_assoc();
+        } else {
+            //si no hay resultados, retornamos false
+            $user = false;
         }
+        //cerramos la coneccion
+        $this->bdConection->closeConnection();
+        //retornamos el usuario
+        return $user;
+    }
 
-        //creamos la funcion para obtener un usuario por su id
-        public function getById($id){
-            //creamos la consulta
-            $sql = "SELECT * FROM user WHERE IdUser = $id";
-            //obtenemos la coneccion
-            $connection = $this->bdConection->getConnection();
-            //ejecutamos la consulta
-            $result = $connection->query($sql);
-            //verificamos que la consulta haya traido un resultado
-            if($result && $result->num_rows > 0){
-                //obtenemos el usuario
-                $user = $result->fetch_assoc();
-            }else{
-                //si no hay resultados, retornamos false
-                $user = false;
-            }
-            //cerramos la coneccion
-            $this->bdConection->closeConnection();
-            //retornamos el usuario
-            return $user;
-        }
-        
-        //creamos la funcion para insertar un usuario
-        public function insert($user){
-            //creamos la consulta
-            $sql = "INSERT INTO user 
+    //creamos la funcion para insertar un usuario
+    public function insert($user)
+    {
+        //creamos la consulta
+        $sql = "INSERT INTO user 
             (Usuario, Nombre, ApPaterno, ApMaterno, Password, Sexo, FchNacimiento, Avatar) 
-            VALUES ('". $user['usuario'] . "', '" . $user['nombre'] . "', 
+            VALUES ('" . $user['usuario'] . "', '" . $user['nombre'] . "', 
             '" . $user['appaterno'] . "', '" . $user['apmaterno'] . "', 
             '" . $user['password'] . "','" . $user['sexo'] . "','" . $user['fchnac'] . "','" . $user['avatar'] . "')";
-            
-            //obtenemos la coneccion
-            $connection = $this->bdConection->getConnection();
-            //ejecutamos la consulta
-            $result = $connection->query($sql);
-            //verificamos que la consulta se haya ejecutado correctamente
-            if($result){
-                //obtenemos el id del usuario insertado
-                $id = $connection->insert_id;
-            }else{
-                //si no se ejecuto correctamente, retornamos false
-                $id = false;
-            }
-            //cerramos la coneccion
-            $this->bdConection->closeConnection();
-            //retornamos el id del usuario insertado
-            return $id;
+
+        //obtenemos la coneccion
+        $connection = $this->bdConection->getConnection();
+        //ejecutamos la consulta
+        $result = $connection->query($sql);
+        //verificamos que la consulta se haya ejecutado correctamente
+        if ($result) {
+            //obtenemos el id del usuario insertado
+            $id = $connection->insert_id;
+        } else {
+            //si no se ejecuto correctamente, retornamos false
+            $id = false;
         }
+        //cerramos la coneccion
+        $this->bdConection->closeConnection();
+        //retornamos el id del usuario insertado
+        return $id;
+    }
 
-        //creamos la funcion para actualizar un usuario
-        public function update($user){
+    //creamos la funcion para actualizar un usuario
+    public function update($user)
+    {
 
-            //creamos la consulta para actualizar el usuario existente excepto el id
-            $sql = "UPDATE User SET Usuario = '" 
-            . $user['usuario'] . "', Nombre = '" . $user['nombre'] 
-            . "', ApPaterno = '" . $user['appaterno'] 
-            . "', ApMaterno = '" . $user['apmaterno'] 
-            . "', Password = '" . $user['password'] 
-            . "', Sexo = '" . $user['sexo'] 
-            . "', FchNacimiento = '" . $user['fchnac'] 
+        //creamos la consulta para actualizar el usuario existente excepto el id
+        $sql = "UPDATE User SET Usuario = '"
+            . $user['usuario'] . "', Nombre = '" . $user['nombre']
+            . "', ApPaterno = '" . $user['appaterno']
+            . "', ApMaterno = '" . $user['apmaterno']
+            . "', Password = '" . $user['password']
+            . "', Sexo = '" . $user['sexo']
+            . "', FchNacimiento = '" . $user['fchnac']
             . "', Avatar = '" . $user['avatar']
             . "' WHERE IdUser = " . $user['IdUser'];
 
-            //obtenemos la coneccion
-            $connection = $this->bdConection->getConnection();
-            //ejecutamos la consulta
-            $result = $connection->query($sql);
-            //verificamos que la consulta se haya ejecutado correctamente
-            if($result){
-                //obtenemos el id del usuario actualizado
-                $id = $connection->insert_id;
-            }else{
-                //si no se ejecuto correctamente, retornamos false
-                $id = false;
-            }
-            //cerramos la coneccion
-            $this->bdConection->closeConnection();
-            //retornamos el id del usuario insertado
-            return $id;
+        //obtenemos la coneccion
+        $connection = $this->bdConection->getConnection();
+        //ejecutamos la consulta
+        $result = $connection->query($sql);
+        //verificamos que la consulta se haya ejecutado correctamente
+        if ($result) {
+            //obtenemos el id del usuario actualizado
+            $id = $connection->insert_id;
+        } else {
+            //si no se ejecuto correctamente, retornamos false
+            $id = false;
         }
-        
-        //creamos la funcion para eliminar un usuario
-        public function delete($id){
-            //creamos la consulta para eliminar el usuario
-            $sql = "DELETE FROM User WHERE IdUser = $id";
-            //obtenemos la coneccion
-            $connection = $this->bdConection->getConnection();
-            //ejecutamos la consulta
-            $result = $connection->query($sql);
-            //verificamos que la consulta se haya ejecutado correctamente
-            if($result){
-                //si se ejecuto correctamente, retornamos true
-                $deleted = true;
-            }else{
-                //si no se ejecuto correctamente, retornamos false
-                $deleted = false;
-            }
-            //cerramos la coneccion
-            $this->bdConection->closeConnection();
-            //retornamos el resultado de la eliminacion
-            return $deleted;
-        }
-
-        //creamos la funcion para obtener un usuario por su usuario y password
-        public function getCredentials($user, $password){
-            //creamos la consulta para obtener el usuario
-            $sql = "SELECT * FROM User WHERE Usuario = '$user' AND Password = '$password'";
-            //obtenemos la coneccion
-            $connection = $this->bdConection->getConnection();
-            //ejecutamos la consulta
-            $result = $connection->query($sql);
-            //verificamos que la consulta haya traido un resultado
-            if($result && $result->num_rows > 0){
-                //obtenemos el usuario con el que se va a iniciar sesion
-                $user = $result->fetch_assoc();
-            }else{
-                //si no hay resultados, retornamos false
-                $user = false;
-            }
-            //cerramos la coneccion
-            $this->bdConection->closeConnection();
-            //retornamos el usuario
-            return $user;
-        }
-
-        
+        //cerramos la coneccion
+        $this->bdConection->closeConnection();
+        //retornamos el id del usuario insertado
+        return $id;
     }
-?>
+
+
+
+    //creamos la funcion para eliminar un usuario
+    public function delete($id)
+    {
+        $sql = "DELETE FROM user WHERE IdUser = $id";
+        $connection = $this->bdConection->getConnection();
+        $result = $connection->query($sql);
+        if ($result) {
+            $deleted = true;
+        } else {
+            $deleted = false;
+        }
+        $this->bdConection->closeConnection();
+        return $deleted;
+    }
+
+    //creamos la funcion para obtener un usuario por su usuario y password
+    public function getCredentials($user, $password)
+    {
+        //creamos la consulta para obtener el usuario
+        $sql = "SELECT * FROM User WHERE Usuario = '$user' AND Password = '$password'";
+        //obtenemos la coneccion
+        $connection = $this->bdConection->getConnection();
+        //ejecutamos la consulta
+        $result = $connection->query($sql);
+        //verificamos que la consulta haya traido un resultado
+        if ($result && $result->num_rows > 0) {
+            //obtenemos el usuario con el que se va a iniciar sesion
+            $user = $result->fetch_assoc();
+        } else {
+            //si no hay resultados, retornamos false
+            $user = false;
+        }
+        //cerramos la coneccion
+        $this->bdConection->closeConnection();
+        //retornamos el usuario
+        return $user;
+    }
+}
